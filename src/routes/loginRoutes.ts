b55d1293e9601c44da12217ced90e8e5cc44fbda
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-04 20:59:33 +0700

    add_requireAuthFunc

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,18 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.isLoggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send("Not Permitted");
+}
 const router = Router();
 
 router.get("/login", (req: Request, res: Response) => {
@@ -30,11 +39,37 @@ router.post("/login", (req: RequestWithBody, res: Response) => {
     email === "giadieuly@gmail.com" &&
     password === "password"
   ) {
-    req.session = { loggedIn: true };
+    req.session = { isLoggedIn: true };
     res.redirect("/");
   } else {
     res.send("Invalid email or password");
   }
 });
 
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.isLoggedIn) {
+    res.send(`
+            <div>
+                <div>You are logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+        `);
+  } else {
+    res.send(`
+        <div>
+            <div>You are not logged in</div>
+            <a href="/login">Login</a>
+        </div>
+    `);
+  }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = null;
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected route, logged in user");
+});
 export { router };
