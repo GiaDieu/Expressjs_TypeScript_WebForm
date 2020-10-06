35e5dd6cc2caaa5ef7b98b41cc7c7921d4f7ffbe
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-06 19:55:47 +0700

    update_compiledJsFiles

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -14,8 +14,19 @@ var router = express_1.Router();
 exports.router = router;
 router.post("/login", function (req, res) {
     var _a = req.body, email = _a.email, password = _a.password;
-    //TypeScript does not give us indication, ideally, req.body contains the Object that might or might not have properties
-    //Solution: create interface RequestWithBody
+    // Understanding body-parser
+    // When users filled on the form and click the button, the information will go into HTTP Request {Headers: ...., and body of HTTP Request {email=email, password=password} and
+    // generate these information to Express Server as Request Object {id: number, hostname:string} and other properties.
+    // By default, Request Object in Express does not have a body property, instead the Request Object of Express will pass to the middleware body-parser.
+    // and then body-parser is going to inspect the incoming HTTP REQUEST whether there is the actual body of HTTP Request or not attached to it and then body-parser is going to parse those information in STRING 'email=email&password=password' and
+    // attach it to the Request Object of Express Server:
+    //{ id: number,
+    //  hostname:string,
+    //  body: {email: 'email', password: 'password'}
+    //}
+    // NOTE:
+    // TypeScript does not give us indication, ideally, req.body contains the Object that might or might not have properties.
+    // Solution: create interface RequestWithBody.
     if (email &&
         password &&
         email === "giadieuly@gmail.com" &&
