c777f0d84daeaa5099e35d90a6f24c00b1caedcd
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-06 19:08:36 +0700

    Refactoring

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -15,31 +15,34 @@ function requireAuth(req: Request, res: Response, next: NextFunction): void {
 }
 const router = Router();
 
-router.get("/login", (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-        <div>
-            <label>Email</label>
-            <input name="email" type="email" />
-        </div>
-        <div>
-            <label>Password</label>
-            <input name="password" type="password" />
-        </div>
-        <button>Submit</button>
-    </form>
-  `);
-});
-
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
+
+  // Understanding body-parser
+  // When users filled on the form and click the button, the information will go into HTTP Request {Headers: ...., and body of HTTP Request {email=email, password=password} and
+  // generate these information to Express Server as Request Object {id: number, hostname:string} and other properties.
+
+  // By default, Request Object in Express does not have a body property, instead the Request Object of Express will pass to the middleware body-parser.
+  // and then body-parser is going to inspect the incoming HTTP REQUEST whether there is the actual body of HTTP Request or not attached to it and then body-parser is going to parse those information in STRING 'email=email&password=password' and
+  // attach it to the Request Object of Express Server:
+
+  //{ id: number,
+  //  hostname:string,
+  //  body: {email: 'email', password: 'password'}
+  //}
+
+  // NOTE:
+  // TypeScript does not give us indication, ideally, req.body contains the Object that might or might not have properties.
+  // Solution: create interface RequestWithBody.
   if (
     email &&
     password &&
     email === "giadieuly@gmail.com" &&
     password === "password"
   ) {
+    //mark this person as logged in
     req.session = { isLoggedIn: true };
+    //redirect them to the root route
     res.redirect("/");
   } else {
     res.send("Invalid email or password");
