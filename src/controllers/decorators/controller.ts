872576ad7ea6e5a9695940cde072746495bd63c3
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-06 21:42:58 +0700

    update_controller.ts_middlewares

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
-import { Methods } from "./Methods";
+import { Methods } from "./Methods"; // enum
+import { MetadataKeys } from "./MetadataKeys"; // enum
+import { use } from "./use";
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
@@ -9,19 +11,27 @@ export function controller(routePrefix: string) {
     for (let key in target.prototype) {
       // key === 'getLogin'
       const routeHandler = target.prototype[key]; // example: 'getLogin' method
-      const path = Reflect.getMetadata("path", target.prototype, key); // '/login'
-      const method: Methods = Reflect.getMetadata(
-        "method",
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
         target.prototype,
         key
-      );
+      ); // '/login'
+
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      ); // 'get'
+
+      const middlewares =
+        Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
 
       // Look at here how flexible, in theory, the method variable will be 'get','put','post' and so on...
       if (path) {
         // refactoring a small piece of code here
         // router.get(`${routePrefix}${path}`, routeHandler);
         // router.get === router[method] == method == 'get'
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
       }
     }
   };
