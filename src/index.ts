9bca7d2c2f12e7b828884d8db64c442668eb1d98
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-07 16:19:02 +0700

    update_index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import { router } from "./routes/loginRoutes";
 import bodyParser from "body-parser"; // it is a middleware
 import cookieSession from "cookie-session";
 import { AppRouter } from "./AppRouter";
 import "./controllers/loginController";
+import "./controllers/rootController";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["default"] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3001, () => {
