5ba54b8573675bbf3c9b53dfc5aa3060a6cb094b
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-07 16:19:35 +0700

    update_controller.ts

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -2,36 +2,66 @@ import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
 import { Methods } from "./Methods"; // enum
 import { MetadataKeys } from "./MetadataKeys"; // enum
-import { use } from "./use";
+import { Request, Response, NextFunction, RequestHandler } from "express";
 
+function bodyValidators(keys: string): RequestHandler {
+  return function (req: Request, res: Response, next: NextFunction) {
+    if (!req.body) {
+      // if user click the button, to check whether the email and password exists
+      res.status(422).send("Invalid Request!");
+      return;
+    }
+    for (let key of keys) {
+      if (!req.body[key]) {
+        res.status(422).send(`Missing Property ${key}`);
+        return;
+      }
+    }
+    next();
+  };
+}
 export function controller(routePrefix: string) {
   return function (target: Function) {
     const router = AppRouter.getInstance();
 
     for (let key in target.prototype) {
       // key === 'getLogin'
-      const routeHandler = target.prototype[key]; // example: 'getLogin' method
+      // key === 'postLogin'
+
+      const routeHandler = target.prototype[key]; // example: 'getLogin' method, 'postLogin' method
       const path = Reflect.getMetadata(
         MetadataKeys.path,
         target.prototype,
         key
-      ); // '/login'
+      ); // '/login','/login'
 
       const method: Methods = Reflect.getMetadata(
         MetadataKeys.method,
         target.prototype,
         key
-      ); // 'get'
+      ); // 'get','post'
 
       const middlewares =
-        Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
+        Reflect.getMetadata(MetadataKeys.middleware, target.prototype, key) ||
+        [];
+
+      const requireBodyProps =
+        Reflect.getMetadata(MetadataKeys.validator, target.prototype, key) ||
+        [];
+
+      const validator = bodyValidators(requireBodyProps);
 
       // Look at here how flexible, in theory, the method variable will be 'get','put','post' and so on...
       if (path) {
         // refactoring a small piece of code here
         // router.get(`${routePrefix}${path}`, routeHandler);
         // router.get === router[method] == method == 'get'
-        router[method](`${routePrefix}${path}`, ...middlewares, routeHandler);
+        router[method](
+          `${routePrefix}${path}`,
+          ...middlewares,
+          validator,
+          routeHandler
+        );
       }
     }
   };
