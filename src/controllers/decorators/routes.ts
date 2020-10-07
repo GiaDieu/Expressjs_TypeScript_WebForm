e6cfda770b32ccbc4d5c4a4b3e11af3af128b4d1
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-10-07 16:30:35 +0700

    update_interfaceInRoutes

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { Methods } from "./Methods"; // enum
 import { MetadataKeys } from "./MetadataKeys"; // enum
+import { RequestHandler } from "express";
 // We Repeat DRY principles
 // https://github.com/kaoengine/kao-pragmatic-progamming#5-your-knowledge-portfolio
 // please check our github repo, our Team has summarized some contents of this book Programatic Programmer
@@ -22,11 +23,19 @@ import { MetadataKeys } from "./MetadataKeys"; // enum
 //Specifically, we create factory decorator
 //https://www.typescriptlang.org/docs/handbook/decorators.html
 
+interface routeHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 function routeBinder(method: string) {
   // method: get/put/post/or patch
   return function (path: string) {
     // path = '/login'
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: routeHandlerDescriptor) {
+      //PropertyDescriptor : can be an Object that having writable, configurable,enumrable and VALUE properties
+      //Value Property reference to actual some functions
+      //interface tells us that if there is any value, you have to satisfy the Request Handler from Express
+      //look at examples in loginController.ts
+
       Reflect.defineMetadata(MetadataKeys.path, path, target, key); // key === 'getLogin'
       Reflect.defineMetadata(MetadataKeys.method, method, target, key);
       // this indicate the method that we want this router handler to assign into router
